c044b76cb22d9b0db0e8ef9f3172de12977d2b81
dcassinotti <damiancassinotti.33@gmail.com>
2017-04-20 21:53:35 -0300

    Arreglos en kosaraju.

--- kosaraju.py
+++ kosaraju.py
@@ -1,38 +1,40 @@
 from readGraph import *
 
-def kosaraju(path):
-	graph = createGraph(path, True)
-	visitados = [False for i in range(graph.cantidadVertices())]
-	L = []
-	args = graph.iterarSobreNodos(visitar, [graph, visitados, L])
-	visitados = args[1]
-	L = args[2]	
-	asignaciones = {}
-	visitados = [False for i in range(graph.cantidadVertices())]
-	for nodo in L:
-		args = asignar(nodo, [asignaciones, visitados, graph, nodo])
-	print "Termino la ejecucion."
-	print "Resultados:"
-	print "asignaciones:", asignaciones
+class Kosaraju(object):
 
-def visitar(nodo, args):
-	if not args[1][nodo]:
-		args[1][nodo] = True
-		args = args[0].iterarSobreAdyacentes(nodo, visitar, args)
-		args[2] = [nodo] + args[2]
-	return args
+	def __init__(self, path):
+		graph = createGraph(path, True)
+		visitados = [False for i in range(graph.cantidadVertices())]
+		L = []
+		args = graph.iterarSobreNodos(self.__visitar, [graph.iterarSobreAdyacentes, visitados, L])
+		visitados = args[1]
+		L = args[2]	
+		asignaciones = {}
+		visitados = [False for i in range(graph.cantidadVertices())]
+		for nodo in L:
+			args = self.__asignar(nodo, [asignaciones, visitados, graph.iterarSobreIncidentes, nodo])
+		self.componentesConexas = asignaciones.values()
 
-def asignar(nodo1, args):
-	if not args[1][nodo1]:
-		args[1][nodo1] = True
-		if(args[3] in args[0]):
-			args[0][args[3]].append(nodo1)
-		else:
-			args[0][args[3]] = [nodo1]
-		args = args[2].iterarSobreIncidentes(nodo1, asignar, args)
-	return args
+	def getCantidadSCC(self):
+		return len(self.componentesConexas)
 		
+	def getSCC(self, id):
+		return self.componentesConexas[id]
 
+	def __asignar(self, nodo1, args):
+		if not args[1][nodo1]:
+			args[1][nodo1] = True
+			if(args[3] in args[0]):
+				args[0][args[3]].append(nodo1)
+			else:
+				args[0][args[3]] = [nodo1]
+			args = args[2](nodo1, self.__asignar, args)
+		return args
+		
+	def __visitar(self, nodo, args):
+			if not args[1][nodo]:
+				args[1][nodo] = True
+				args = args[0](nodo, self.__visitar, args)
+				args[2] = [nodo] + args[2]
+			return args		
 
-
-kosaraju("ejercicio3/d1.txt")
