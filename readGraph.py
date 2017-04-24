6b4bf113ca70a93e9d8a0d89182fe5937e63b02f
dcassinotti <damiancassinotti.33@gmail.com>
2017-04-23 21:52:31 -0300

    Agrego la creación del grafo transpuesto para iterar sobre los incidentes

--- readGraph.py
+++ readGraph.py
@@ -12,3 +12,16 @@ def createGraph(path, dirigido):
 		if(not dirigido):
 			grafo.agregarArista(nodos[1], nodos[0])
 	return grafo
+	
+def createTransposeGraph(path, dirigido):
+	file = open(path, "r")
+	cantidadVertices = file.readline()
+	grafo = Grafo(int(cantidadVertices))
+	cantidadAristas = int(file.readline())
+	for x in range(cantidadAristas):
+		linea = file.readline()
+		nodos = [int(x) for x in linea.split(" ")]
+		grafo.agregarArista(nodos[1], nodos[0])
+		if(not dirigido):
+			grafo.agregarArista(nodos[0], nodos[1])
+	return grafo
